b0ffb2166ceb1b427ea1ad44c7b1ae63b4d704b1
bekshodbaxronov80-max <bekshodbaxronov80@gmail.com>
2026-05-22 19:23:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
 import os
-BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
-if not BOT_TOKEN:
-    try:
-        with open(".env", "r") as f:
-            for line in f:
-                if line.startswith("TELEGRAM_TOKEN="):
-                    BOT_TOKEN = line.split("=", 1)[1].strip()
-    except: pass
+
+BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8260286135:AAHqT9xLpVkM1rBzY8cFgHuJwN3oKmR5sD")
