c95bf5f3e6577d22df1c703a5b661888c6f50793
bekshodbaxronov80-max <bekshodbaxronov80@gmail.com>
2026-05-22 19:24:37 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 
-BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8260286135:AAHqT9xLpVkM1rBzY8cFgHuJwN3oKmR5sD")
+BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8956169061:AAGk93ybwSEsEoyb8tbVdi8gX7xWbunKap8")
